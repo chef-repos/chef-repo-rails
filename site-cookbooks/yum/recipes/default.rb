b2d7cbae12e935588fe8c4bbd3c1259f3f633e90
kouhot <kouhot6210@gmail.com>
2016-06-22 13:07:17 +0900

    create recipes

--- site-cookbooks/yum/recipes/default.rb
+++ site-cookbooks/yum/recipes/default.rb
@@ -6,3 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+yum_globalconfig '/etc/yum.conf' do
+  cachedir node['yum']['main']['cachedir']
+  keepcache node['yum']['main']['keepcache']
+  debuglevel node['yum']['main']['debuglevel']
+  exclude node['yum']['main']['exclude']
+  logfile node['yum']['main']['logfile']
+  exactarch node['yum']['main']['exactarch']
+  obsoletes node['yum']['main']['obsoletes']
+  proxy node['yum']['main']['proxy']
+  installonly_limit node['yum']['main']['installonly_limit']
+  installonlypkgs node['yum']['main']['installonlypkgs']
+  installroot node['yum']['main']['installroot']
+  distroverpkg node['yum']['main']['distroverpkg']
+  releasever node['yum']['main']['releasever']
+  action :create
+end
+
+execute "yum-update" do
+  user "root"
+  command "yum -y update"
+  action :run
+end
+
+%w{gcc gcc-c++ make wget patch svn autoconf byacc sqlite-devel zlib-devel openssl-devel ImageMagick-devel mysql-devel telnet curl readline-devel ncurses-devel gdbm-devel  freetype-devel fontconfig-devel libxml2 libxml2-devel libxslt libxslt-devel}.each do |p|
+  package p do
+    action :install
+  end
+end
